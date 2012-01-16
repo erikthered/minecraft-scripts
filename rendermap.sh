eccf2ac73c5ff011ce24ccb2c058a4170783979a
Erik Nelson <erik.david.nelson@gmail.com>
2012-01-16 16:25:12 -0500

    Removed run_as reference from rendermap.sh

--- rendermap.sh
+++ rendermap.sh
@@ -7,13 +7,13 @@ TMP_FOLDER="/home/minecraft/tmp" # Temp folder to copy the world to
 OUTPUT="/home/minecraft/map" # Output folder for overviewer
 
 # Disable saving
-run_as "screen -p 0 -S minecraft -X eval 'stuff save-off\015'"	
+screen -p 0 -S minecraft -X eval 'stuff save-off\015'
 
 # Copy world directory to temp folder
 cp -pr $MINECRAFT_FOLDER/$WORLD $TMP_FOLDER/$WORLD
 
 # Enable Saving
-run_as "screen -p 0 -S minecraft -X eval 'stuff save-on\015'"	
+screen -p 0 -S minecraft -X eval 'stuff save-on\015'
 
 # Run the Overviewer
 echo "Starting render."
