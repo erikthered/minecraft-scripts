f407c0fe78c465e27b334ef5dd63456efac6b13f
Erik Nelson <erik.david.nelson@gmail.com>
2012-01-16 16:22:36 -0500

    Updated map render script to copy to a temp folder before rendering.

--- rendermap.sh
+++ rendermap.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
 
-# Location of Minecraft server folder
-MINECRAFT_FOLDER=/home/minecraft/minecraft_server
-WORLD="world"
+# Relevant locations for the backup
+MINECRAFT_FOLDER="/home/minecraft/minecraft_server" # Server directory
+WORLD="world" # World name
+TMP_FOLDER="/home/minecraft/tmp" # Temp folder to copy the world to
+OUTPUT="/home/minecraft/map" # Output folder for overviewer
 
-# Location of output folder
-OUTPUT=/home/minecraft/map
+# Disable saving
+run_as "screen -p 0 -S minecraft -X eval 'stuff save-off\015'"	
+
+# Copy world directory to temp folder
+cp -pr $MINECRAFT_FOLDER/$WORLD $TMP_FOLDER/$WORLD
+
+# Enable Saving
+run_as "screen -p 0 -S minecraft -X eval 'stuff save-on\015'"	
 
 # Run the Overviewer
-overviewer.py $MINECRAFT_FOLDER/$WORLD $OUTPUT
+echo "Starting render."
+overviewer.py $TMP_FOLDER/$WORLD $OUTPUT
+echo "Rendering complete."
+
+# Cleanup temp files
+echo "Cleaning up temp files"
+rm -rf $TMP_FOLDER/$WORLD
+
+echo "Done"
