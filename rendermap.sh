6f2867a93c32989667b30a8e3261cf6c208a0912
Erik Nelson <erik.david.nelson@gmail.com>
2012-01-16 16:44:05 -0500

    Added some more console output to the rendering script.

--- rendermap.sh
+++ rendermap.sh
@@ -7,6 +7,7 @@ TMP_FOLDER="/home/minecraft/tmp" # Temp folder to copy the world to
 OUTPUT="/home/minecraft/map" # Output folder for overviewer
 
 # Disable saving
+echo "Disabling saves on the server"
 screen -p 0 -S minecraft -X eval 'stuff save-off\015'
 
 # Copy world directory to temp folder
@@ -14,6 +15,7 @@ cp -pr $MINECRAFT_FOLDER/$WORLD $TMP_FOLDER/$WORLD
 
 # Enable Saving
 screen -p 0 -S minecraft -X eval 'stuff save-on\015'
+echo "Saves reenabled on the server"
 
 # Run the Overviewer
 echo "Starting render."
